a30e6555d71305c7c5ef67ecea21c05423c3c699
Tahmidmobassir <tahmidmobassirkhan@gmail.com>
2025-03-22 21:42:20 +0600

    7th Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function showPromotion() {
+    const promoCode = "Candle15";
+    alert(`Use promo code ${promoCode} at checkout to get 15% off your first order!`);
+}
+    document.querySelector('.secondary-button').style.display = 'none';
